bba496a754c6a6b99088816203ae7effdfa94148
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-11-10 15:47:17 +0900

    feature: 係数追加

--- lib/sqrt/object.rb
+++ lib/sqrt/object.rb
@@ -1,10 +1,11 @@
 class Sqrt::Object
   # x type expects only Integer, Float, Rela in v0.1.0
-  def initialize(x)
+  def initialize(coefficient, x)
+    @coefficient = coefficient
     @value = x
   end
 
   def to_f
-    @value.positive? ?  Math.sqrt(@value) : Complex(0, Math.sqrt(-@value))
+    @coefficient * (@value.positive? ? Math.sqrt(@value) : Complex(0, Math.sqrt(-@value)))
   end
 end
--- test/sqrt_test.rb
+++ test/sqrt_test.rb
@@ -6,20 +6,25 @@ class SqrtTest < Minitest::Test
   end
 
   def test_initialize_puts_value_on_variable_target
-    x = Sqrt::Object.new(5)
+    x = Sqrt::Object.new(1, 5)
 
+    assert_equal(1, x.instance_variable_get(:@coefficient))
     assert_equal(5, x.instance_variable_get(:@value))
   end
 
   def test_to_f_returns_sqrt_value
-    x1 = Sqrt::Object.new(5)
-    x2 = Sqrt::Object.new(4)
-    x3 = Sqrt::Object.new(2/3r)
-    x4 = Sqrt::Object.new(-1)
+    x1 = Sqrt::Object.new(1, 5)
+    x2 = Sqrt::Object.new(1, 4)
+    x3 = Sqrt::Object.new(1, 2/3r)
+    x4 = Sqrt::Object.new(1, -1)
+    x5 = Sqrt::Object.new(2, 5)
 
     assert_equal(Math.sqrt(5), x1.to_f)
     assert_equal(2, x2.to_f)
     assert_equal(Math.sqrt(2/3r), x3.to_f)
     assert_equal(1i, x4.to_f)
+    assert_equal(2 * Math.sqrt(5), x5.to_f)
+  end
+
   end
 end
